afc89065f8d34ba8949d2ca8d32941033e500621
Devaka Kumarasinghe <k.devaka@directfn.com>
2024-05-25 16:18:30 -0400

    ticket card

--- src/moonshot_frontend/src/components/Marketplace/Marketplace.jsx
+++ src/moonshot_frontend/src/components/Marketplace/Marketplace.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-
+import React from "react";
+import { TicketCard } from "./TicketCard";
 
 export const Marketplace = () => {
-
-    return (
-        <main>
+  return (
+    <main>
       <h1>Marketplace</h1>
-        </main>
-    );
-    }
+
+      <TicketCard />
+    </main>
+  );
+};
