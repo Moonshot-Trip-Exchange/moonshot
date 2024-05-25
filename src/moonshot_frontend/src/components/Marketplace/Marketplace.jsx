10a4d36eabf9e9693284938a7bab9493e99be107
DChasmar <davidchasmar@gmail.com>
2024-05-25 17:25:07 -0400

    Some changes

--- src/moonshot_frontend/src/components/Marketplace/Marketplace.jsx
+++ src/moonshot_frontend/src/components/Marketplace/Marketplace.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { TicketCard } from "./TicketCard";
 import { CardWrapper } from "./assets/CardWrapper";
+import { FilterWrapper } from "./FilterWrapper";
+import { Box } from "@mui/material";
 
 export const Marketplace = () => {
   return (
     <main>
       <h1>Marketplace</h1>
-    
-      <CardWrapper/>
+      <Box display="flex">
+        <FilterWrapper />
+
+        <CardWrapper />
+      </Box>
+      ß
     </main>
   );
 };
