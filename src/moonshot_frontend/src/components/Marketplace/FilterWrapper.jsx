da5f6fa9dc925f35688e79736312c5ac6cd6fa22
DChasmar <davidchasmar@gmail.com>
2024-05-25 17:28:32 -0400

    Some changes

---
+++ src/moonshot_frontend/src/components/Marketplace/FilterWrapper.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+
+export const FilterWrapper = () => {
+    return (
+        <Box sx={{width: 600}}>
+            <Typography variant="h5">Filters</Typography>
+
+            
+
+        </Box>
+    );
+}
